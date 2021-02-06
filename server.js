e36b14f4ad8639d62141f78716b9a9dcd1fe9175
Patrick <patrick@acsghana.com>
2021-02-06 18:06:19 +0000

    kkk

--- server.js
+++ server.js
@@ -5,20 +5,6 @@ app.use(cors());
 const path = require("path");
 
 
-const whitelist = ['http://localhost:3000'​​]
-const corsOptions = {
-  origin: function (origin, callback) {
-    console.log("** Origin of request " + origin)
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      console.log("Origin acceptable")
-      callback(null, true)
-    } else {
-      console.log("Origin rejected")
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-app.use(cors(corsOptions))
 
 
 if (process.env.NODE_ENV === 'production') {
